c582179500410eb6f963531dea504f872dfc1e6d
Daniel Kneipp <dkdsv@hotmail.com>
2014-09-13 13:06:30 -0300

    Adicionado mais tipos de desenhos para testes.

--- main.cpp
+++ main.cpp
@@ -5,28 +5,74 @@
 
 #define _COLS 100
 #define _ROWS 100
-#define _SIZE 30
+#define _SIZE 50
 
 #define _MAIN_WINDOW_TITLE "Image Test"
 
 using namespace std;
 
+void drawMainDiagonal( cv::Mat& img )
+{
+    float ratio =  (float) _COLS / (float) _ROWS;
+    for( int i = round( (float) _ROWS / 10 ); i < img.rows - round( (float) _ROWS / 10 ); i++ )
+    {
+        img.at< uchar >( i, round( (float) i * ratio ) ) = 255;
+    }
+}
+
 void drawSquare( cv::Mat& img )
 {
-    for( int i = round( _SIZE / 10 ); i < img.rows - round( _SIZE / 10 ); i++ )
+    int colMin = round( (float) _COLS / 10 ), rowMin = round( (float) _ROWS / 10 );
+
+    // Linhas de cima e de baixo
+    for( int i = colMin ; i < img.cols - colMin; i++ )
     {
-        img.at< uchar >( i, i ) = 255;
+        img.at< uchar >( rowMin, i ) = 255;
+        img.at< uchar >( img.rows - rowMin, i ) = 255;
     }
+
+    // Colunas da esquerda e direita
+    for( int i = rowMin ; i < img.rows - rowMin; i++ )
+    {
+        img.at< uchar >( i, colMin ) = 255;
+        img.at< uchar >( i, img.cols - colMin ) = 255;
+    }
+}
+
+// Errors no extremos inferior e direito.
+void drawCircle( cv::Mat& img )
+{
+    int colMin = round( (float) _COLS / 10 ), rowMin = round( (float) _ROWS / 10 );
+    float a = round( (float) _ROWS / 2 );
+    float b = round( (float) _COLS / 2 );
+    int r = round( ( (_COLS - colMin) - colMin) / 2 );
+
+    for( int i = rowMin ; i < img.rows - rowMin; i++ )
+    {
+        for( int j = colMin ; j < img.cols - colMin; j++ )
+        {
+            if(  pow( i - (int) a, 2 )  + pow( j - (int) b, 2 ) >=  pow( r, 2 ) - 70 &&
+                 pow( i - (int) a, 2 )  + pow( j - (int) b, 2 ) <=  pow( r, 2 ) + 70 )
+            {
+                img.at< uchar >( i, j ) = 255;
+            }
+        }
+    }
+
 }
 
 int main()
 {
-    cv::Mat imgTest( _SIZE, _SIZE, CV_8U, cv::Scalar::all( 0 ) );
+    cv::Mat imgTest( _ROWS, _COLS, CV_8U, cv::Scalar::all( 0 ) );
     HoughTransform HT( imgTest.size() );
 
+    //drawMainDiagonal( imgTest );
     drawSquare( imgTest );
+    //drawCircle( imgTest );
+    cv::circle(imgTest, cv::Point(50,50), 40, cv::Scalar::all(255), 1, 1);
+
     HT.compute( imgTest );
-    HT.getBestParamsRT( 4 );
+    HT.getBestParamsRT( 38 );
     cv::Mat HSimg = HT.getHoughSpaceImage();
 
     cv::namedWindow( _MAIN_WINDOW_TITLE );
